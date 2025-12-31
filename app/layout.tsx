93d2f63cac4fcb575e7b6634b9373ccdb588993b
Vulchi Vijaya Kumar <17252359+vulchivijay@users.noreply.github.com>
2025-12-31 18:53:02 +0530

    bing script added

--- app/layout.tsx
+++ app/layout.tsx
@@ -99,6 +99,14 @@ export default async function RootLayout({
             })
           }}
         />
+        {/* Clarity tracking code for https://sanatanadharmam.in/ */}
+        <Script
+          id="bing-script"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){ c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}; t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt"; y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y); })(window, document, "clarity", "script", "uu55kbw70q");`
+          }}
+        />
         <Script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
