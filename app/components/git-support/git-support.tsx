1bd8af287a83a0fe344251013992ecb49870628e
Vulchi Vijaya Kumar <vulchi.vijay@gmail.com>
2026-01-04 15:52:41 +0530

    electrical border added

--- app/components/git-support/git-support.tsx
+++ app/components/git-support/git-support.tsx
@@ -23,7 +23,7 @@ export default function GitSupport({ locale }: { locale?: string }) {
         showGraticule={false}
       />
       <div className={`${styles.gitsupport} md:content-wrapper md:absolute md:top-1/2 md:left-20 md:-translate-y-1/2 md:z-1`}>
-        <h6 className={`title`}>{t('cta.title', loc)}</h6>
+        <h6 className={`title font-light!`}>{t('cta.title', loc)}</h6>
         <p>{t('cta.subtitle', loc)}</p>
         <div className="flex flex-col md:flex-row gap-4">
           <Link href="https://github.com/vulchivijay/first-contributes" target="_blank" className="button inline-block shadow-sm bg-amber-300 hover:bg-amber-400 no-underline">{t('cta.contribute', loc)}</Link>
