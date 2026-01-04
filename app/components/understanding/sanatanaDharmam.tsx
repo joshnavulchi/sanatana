1dfebf12d3dc7453c06eb0b4633db3ceb644b53e
Vulchi Vijaya Kumar <vulchi.vijay@gmail.com>
2026-01-04 20:57:28 +0530

    border animations

--- app/components/understanding/sanatanaDharmam.tsx
+++ app/components/understanding/sanatanaDharmam.tsx
@@ -2,7 +2,6 @@
 import { useT } from '../../hooks/useT';
 import { parseList } from 'lib/parseList';
 import styles from './sanatanadharmam.module.scss';
-import ElectricalBorder from '../electrical-border/electrical';
 
 export default function UnderstandingOfSanatana() {
   const t = useT();
@@ -12,7 +11,7 @@ export default function UnderstandingOfSanatana() {
     <div className={`${styles.understanding} content-wrapper text-center`}>
       {sections.map((section) => (
         <div key={section.id} className={`${styles.sections}`}>
-          <ElectricalBorder title={section.title} />
+          <p className={`${styles.borderbottom} title`}>{section.title}</p>
           <p className={`subtitle`}>{section.content}</p>
           <div className={`cards flex gap-10 md:gap-6`}>
             {section?.items && section?.items.map((topic: any, index: number) => (
