30b853ebd5ee8cffcecde16304e8e275f1ad9fcb
Vulchi Vijaya Kumar <vulchi.vijay@gmail.com>
2026-01-11 20:20:51 +0530

    home page updates

--- app/components/understanding/sanatanaDharmam.tsx
+++ app/components/understanding/sanatanaDharmam.tsx
@@ -3,6 +3,7 @@ import { useT } from '../../hooks/useT';
 import { JSX } from 'react';
 import { parseList } from 'lib/parseList';
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from './sanatanadharmam.module.scss';
 
 export default function UnderstandingOfSanatana() {
@@ -14,21 +15,23 @@ export default function UnderstandingOfSanatana() {
         const level = Math.min(index + 2, 6);
         const Tag = `h${level}` as keyof JSX.IntrinsicElements
         return (<div key={section.id} className={`${styles.sections} text-center`}>
-          <Tag className={`${styles.borderbottom} title`}>{section.title}</Tag>
-          <p className={`description text-left md:indent-16`}>{section.content}</p>
-          <p className={`description text-left md:indent-16`}>{section.description}</p>
+          <div className="mx-auto max-w-2xl">
+            <Tag className={`${styles.borderbottom} title`}>{section.title}</Tag>
+            <p>{section.content}</p>
+          </div>
           <div className={`cards flex gap-10 md:gap-6`}>
             {section?.items && section?.items.map((topic: any, index: number) => (
               <Link key={index} href={topic.href} title={topic.title} className="card w-full md:w-1/4 lg:w-1/4 rounded-2xl shadow-sm hover:shadow-md transition">
-                <div className={``}>
+                <div className="text-center">
+                  {topic.src && <Image src={topic.src} alt={topic.title} width={101} height={101} className="mx-auto object-cover" />}
                   <p className={`font-semibold`}>{topic.title}</p>
-                  <p className={`description text-left`}>{topic.description}</p>
+                  <p>{topic.description}</p>
                 </div>
               </Link>
             ))}
             {section?.points && section?.points.map((point: any, idx: number) => (
               <div key={idx} className={`card w-full md:w-1/4 lg:w-1/4 rounded-2xl shadow-sm hover:shadow-md transition`}>
-                <p className="subtitle font-semibold! mb-0!">{point}</p>
+                <p>{point}</p>
               </div>
             ))}
           </div>
