29022bd8ff49f141292d96a816b4eae04f14f230
Vulchi Vijaya Kumar <17252359+vulchivijay@users.noreply.github.com>
2026-01-29 05:26:12 -0800

    karma page

--- app/philosophy/karma/page.tsx
+++ app/philosophy/karma/page.tsx
@@ -1,6 +1,6 @@
 /* Copyright (c) 2025 sanatanadharmam.in Licensed under SEE LICENSE IN LICENSE. All rights reserved. */
 import { createGenerateMetadata } from '@/lib/pageUtils';
-export const generateMetadata = createGenerateMetadata('karma_philosophy');
+export const generateMetadata = createGenerateMetadata('philosophy_karma');
 
 import KrishnaExplainsFiveKarmasClient from './karmaclient';
 
