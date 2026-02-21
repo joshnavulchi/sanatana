c4fcdad0b03b2346d66bc062a8590dfd048afc52
Vulchi Vijaya Kumar <vulchi.vijay@gmail.com>
2026-02-21 10:43:58 +0530

    design updates bhagavad gita and mahabharata

--- app/scriptures/bhagavadgita/page.tsx
+++ app/scriptures/bhagavadgita/page.tsx
@@ -105,7 +105,7 @@ export default function Page({ searchParams }: any) {
                       <div className="relative z-10 pt-4 flex items-center gap-2 text-amber-800 font-semibold text-sm hover:gap-3 transition-all duration-300">
                         <Link
                           href={`/scriptures/bhagavadgita/part/bhagavadgita_part_${i + 1}`}
-                          className="inline-flex items-center gap-2 bg-gradient-to-r from-amber-500 to-orange-500 hover:from-orange-600 hover:to-amber-600 text-white font-bold px-6 py-2 rounded-xl shadow-lg hover:shadow-xl transition-all duration-300"
+                          className="inline-flex items-center gap-2 bg-gradient-to-r from-amber-500 to-orange-500 hover:from-orange-600 hover:to-amber-600 text-white px-3 py-1 rounded-md shadow-md hover:shadow-xl transition-all duration-300"
                         >
                           <span>Read Part</span>
                           <span className="text-xl">→</span>
