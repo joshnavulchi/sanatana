bda3b2709336717dc5537479927b99f4bc5318f6
Vulchi Vijaya Kumar <17252359+vulchivijay@users.noreply.github.com>
2026-03-13 21:04:57 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,7 +86,7 @@ export default async function RootLayout({
         <Script
           id="jsonld-site"
           type="application/ld+json"
-          strategy="afterInteractive"
+          // strategy="afterInteractive"
           dangerouslySetInnerHTML={renderJsonLdScript(siteJson)}
         />
         <Script
@@ -98,7 +98,7 @@ export default async function RootLayout({
         <Script
           id="jsonld-web"
           type="application/ld+json"
-          strategy="afterInteractive"
+          // strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
               "@context": "https://schema.org",
@@ -118,7 +118,7 @@ export default async function RootLayout({
         <Script
           id="jsonld-organization"
           type="application/ld+json"
-          strategy="afterInteractive"
+          // strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
               "@context": "https://schema.org",
