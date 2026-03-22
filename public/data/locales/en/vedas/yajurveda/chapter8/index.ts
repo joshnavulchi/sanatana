f62e9047641092eaa6b4eae7ce9059eb4fbddc87
Vijaya Kumar V <vulchi.vijay@gmail.com>
2026-03-22 21:00:32 +0530

    vedas, upanishads locales restored

---
+++ public/data/locales/en/vedas/yajurveda/chapter8/index.ts
@@ -0,0 +1,23 @@
+// @ts-nocheck
+// Auto-generated index (JSON-only)
+// Folder: 'en\vedas\yajurveda\chapter8'
+// DO NOT EDIT MANUALLY
+
+import _j0 from './mantra1.json';
+import _j1 from './mantra10.json';
+import _j2 from './mantra11.json';
+import _j3 from './mantra12.json';
+import _j4 from './mantra2.json';
+import _j5 from './mantra3.json';
+import _j6 from './mantra4.json';
+import _j7 from './mantra5.json';
+import _j8 from './mantra6.json';
+import _j9 from './mantra7.json';
+import _j10 from './mantra8.json';
+import _j11 from './mantra9.json';
+
+
+
+const merged = Object.assign({}, _j0, _j1, _j2, _j3, _j4, _j5, _j6, _j7, _j8, _j9, _j10, _j11);
+
+export default merged;
