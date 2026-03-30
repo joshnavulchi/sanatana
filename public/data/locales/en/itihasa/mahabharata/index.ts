a3a45d4d790d17925a60d8ba0d23686edcd9ca0b
Vulchi Vijaya Kumar <vulchi.vijayakumar@hcltech.com>
2026-03-30 14:27:46 +0530

    mahabharata json updates

--- public/data/locales/en/itihasa/mahabharata/index.ts
+++ public/data/locales/en/itihasa/mahabharata/index.ts
@@ -2,11 +2,6 @@
 // Auto-generated index (JSON-only)
 // Folder: 'en\itihasa\mahabharata'
 // DO NOT EDIT MANUALLY
-
 import _j0 from './index.json';
-
-
-
 const merged = Object.assign({}, _j0);
-
 export default merged;
