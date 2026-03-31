db5bdb1f1ecfbe9bb0af1a61735a2a5e85884f97
Vijaya Kumar V <vulchi.vijay@gmail.com>
2026-03-31 21:50:31 +0530

    itihasa file renames

--- app/itihasa/bhagavadgita/page.tsx
+++ app/itihasa/bhagavadgita/page.tsx
@@ -1,8 +1,8 @@
 /* Copyright (c) 2025 sanatanadharmam.in Licensed under SEE LICENSE IN LICENSE. All rights reserved. */
-import BhagavadGitaClient from './BhagavadgitaClient';
 import { createGenerateMetadata } from '@lib/pageUtils';
-
 export const generateMetadata = createGenerateMetadata('itihasa/bhagavadgita/index');
+
+import BhagavadGitaClient from './BhagavadgitaClient';
 
 export default function Page() {
   return (
