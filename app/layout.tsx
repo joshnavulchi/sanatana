80db6f92cb0934f72b20b30468a8bb87675674ad
Vulchi Vijaya Kumar <vulchi.vijay@gmail.com>
2026-04-27 08:10:57 -0700

    Fix CSS file reference and update audit report data

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,7 +94,7 @@ export default async function RootLayout({
                 var l=document.createElement('link');
                 l.rel='preload';
                 l.as='style';
-                l.href='/globals.from-scss.51c44e30.51c44e30.51c44e30.css';
+                l.href='/globals.from-scss.51c44e30.51c44e30.51c44e30.51c44e30.css';
                 l.onload=function(){this.onload=null;this.rel='stylesheet'};
                 document.head.appendChild(l);
               })();
@@ -130,7 +130,7 @@ export default async function RootLayout({
             `
           }}
         />
-        <noscript><link rel="stylesheet" href="/globals.from-scss.51c44e30.51c44e30.51c44e30.css" /></noscript>
+        <noscript><link rel="stylesheet" href="/globals.from-scss.51c44e30.51c44e30.51c44e30.51c44e30.css" /></noscript>
         {/* JSON-LD structured data for Website/Organization */}
         <meta name="google-site-verification" content="kxWcUTvXW7Ag5H1jtSxNuYUoKcWm-sq0on2s-h5ILF8" />
         {/* Canonical global JSON-LD: single WebSite + Organization definitions */}
